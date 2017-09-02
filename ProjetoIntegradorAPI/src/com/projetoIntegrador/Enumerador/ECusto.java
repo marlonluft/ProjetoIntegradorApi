73b0f7492331c7a532425ce466edc18d5d0002cc
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2017-09-01 23:43:08 -0300

    Tipo de custos

--- ProjetoIntegradorAPI/src/com/projetoIntegrador/Enumerador/ECusto.java
+++ ProjetoIntegradorAPI/src/com/projetoIntegrador/Enumerador/ECusto.java
@@ -2,4 +2,27 @@ package com.projetoIntegrador.Enumerador;
 
 public enum ECusto {
 
+	PASSAGEM(0),
+	HOSPEDAGEM(1),
+	ALIMENTACAO(2),
+	TRANSPORTE(3),
+	OUTROS(4);
+	
+	private final int index;
+	private ECusto(int valor){
+		this.index = valor;
+	}
+	
+	public int getIndex(){
+		return index;
+	}
+	
+	public static ECusto getEnum(int value) throws Exception{
+        for (ECusto e:ECusto.values()) {
+            if(e.getIndex() == value)
+                return e;
+        }
+        
+        throw new Exception("Falha ao recuperar Status.");
+    }	
 }
