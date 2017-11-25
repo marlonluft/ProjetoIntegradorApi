9593ccfb1bd8e5b04d8c12939d9ee917e1619368
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2017-11-25 21:20:22 -0200

    Ajuste

--- ProjetoIntegradorAPI/src/com/projetoIntegrador/Util/CORSFilter.java
+++ ProjetoIntegradorAPI/src/com/projetoIntegrador/Util/CORSFilter.java
@@ -10,7 +10,7 @@ public class CORSFilter implements ContainerResponseFilter
     {
         response.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
         response.getHttpHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        response.getHttpHeaders().add("Access-Control-Allow-Credentials", "true");
+        //response.getHttpHeaders().add("Access-Control-Allow-Credentials", "true");
         response.getHttpHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 
         return response;
